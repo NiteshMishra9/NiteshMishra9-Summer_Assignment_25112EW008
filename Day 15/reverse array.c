dc0352c769048c94588dbac127b51e749370ff70
Nitesh Mishra <mishrao6010@gmail.com>
2026-06-19 12:54:40 +0530

    completed

---
+++ Day 15/reverse array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array in reverse order:\n");
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
